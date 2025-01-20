63f4fab31dbe62653ee33a214792a854515d37f6
Anurag Yadav <anuragvnm01@gmail.com>
2025-01-20 23:59:19 +0530

    intial commit

---
+++ py/string .py
@@ -0,0 +1,15 @@
+str = " I am studying python from apna collage"
+str = str.capitalize()
+print(str)
+
+str = "i am studing python from apna collage"
+print(str.replace("python","javascript"))
+
+str = " I am studying python from apna collage"
+print(str.find("Q"))
+
+#count function
+str = " I am from studying python from apna collage"
+print(str.count("o"))
+
+ 
