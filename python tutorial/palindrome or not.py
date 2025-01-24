f8571e4af7df80a906a7d2cd7eff96ac4052da3b
Anurag Yadav <anuragvnm01@gmail.com>
2025-01-24 23:56:04 +0530

    intial commit

---
+++ python tutorial/palindrome or not.py
@@ -0,0 +1,12 @@
+n = int(input("enter n digit number\n"))
+num = n 
+rev = 0
+while n>0:
+    d=n%10
+    rev=rev*10+d
+    n=n//10
+print("reverse",rev)
+if rev==num:
+    print("palindrome")
+else:
+    print("not palindrome")
