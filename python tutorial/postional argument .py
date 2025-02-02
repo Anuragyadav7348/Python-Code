32fcac983470eff392246ed54e591f95ea383b0c
Anurag Yadav <anuragvnm01@gmail.com>
2025-02-03 00:47:53 +0530

    name change

--- python tutorial/postional argument .py
+++ python tutorial/postional argument .py
@@ -2,5 +2,5 @@ def greet(name, age):
     print(f"Hello, {name}! You are {age} years old.")
 
 # Calling the function with positional arguments
-greet("Anurag", 30)
+greet("pallu", 30)
 greet("Bob", 25)
