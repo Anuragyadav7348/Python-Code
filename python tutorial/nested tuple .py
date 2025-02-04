f713f56d0d1620b3c58795ea02bdf26f99552ece
Anurag Yadav <anuragvnm01@gmail.com>
2025-02-04 15:22:42 +0530

    intial commit

---
+++ python tutorial/nested tuple .py
@@ -0,0 +1,10 @@
+# Nested tuple
+nested_tuple = (1, (2, 3), 4)
+
+# Unpacking the nested tuple
+a, (b, c), d = nested_tuple
+
+print(a)  # Output: 1
+print(b)  # Output: 2
+print(c)  # Output: 3
+print(d)  # Output: 4
